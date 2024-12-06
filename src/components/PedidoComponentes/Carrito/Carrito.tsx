9f4e97646fdfaa8f8ec2f2e62731c01031f969cc
Luc1aCh4c0ma <132414863+Luc1aCh4c0ma@users.noreply.github.com>
2024-12-06 20:00:14 -0300

    carrito

--- src/components/PedidoComponentes/Carrito/Carrito.tsx
+++ src/components/PedidoComponentes/Carrito/Carrito.tsx
@@ -2,16 +2,14 @@ import React, { useState } from "react";
 import { IconButton, Badge, Box, Modal, Typography, Button } from "@mui/material";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import DeleteIcon from "@mui/icons-material/Delete";
-import "./Carrito.css"; // Archivo CSS para los estilos
+import "./Carrito.css";
 import { useResumen } from "../../../context/ResumenContext";
-import axios from 'axios';
-
+import axios from "axios";
 
 interface CarritoProps {
   metodoPago: string;
   onPedidoConfirmado: (pedido: { id: number; items: any; total: number }) => void;
 }
-
 
 const Carrito: React.FC<CarritoProps> = ({ onPedidoConfirmado }) => {
   const { resumen, eliminarItem } = useResumen();
@@ -28,7 +26,6 @@ const Carrito: React.FC<CarritoProps> = ({ onPedidoConfirmado }) => {
     );
   };
 
-
   const handleConfirmarPedido = async () => {
     const pedido = {
       items: Object.values(resumen).map((item) => ({
@@ -38,24 +35,24 @@ const Carrito: React.FC<CarritoProps> = ({ onPedidoConfirmado }) => {
       })),
       total: calcularTotal(),
       metodoPago,
-      estado: 'pendiente',
+      estado: "pendiente",
     };
-  
-    console.log('Datos enviados al backend:', pedido);
-  
+
+    console.log("Datos enviados al backend:", pedido);
+
     try {
-      const response = await axios.post('https://tesis-back-production-8e0c.up.railway.app/pedidos', pedido);
-      onPedidoConfirmado(response.data); // Confirma el pedido
-      setModalAbierto(true); // Abre un modal de confirmación
+      const response = await axios.post(
+        "https://tesis-back-production-8e0c.up.railway.app/pedidos",
+        pedido
+      );
+      onPedidoConfirmado(response.data);
+      setModalAbierto(true); // Abre el modal de confirmación
+      setCarritoAbierto(false); // Minimiza el carrito
     } catch (error) {
-      console.error('Error al confirmar el pedido:', error);
-      alert('No se pudo confirmar el pedido.');
+      console.error("Error al confirmar el pedido:", error);
+      alert("No se pudo confirmar el pedido.");
     }
   };
-  
-  
-  
-
 
   const handleCloseModal = () => {
     setModalAbierto(false);
@@ -65,15 +62,12 @@ const Carrito: React.FC<CarritoProps> = ({ onPedidoConfirmado }) => {
   return (
     <Box>
       {/* Botón flotante con el ícono del carrito */}
-     <div>
-      <IconButton
-        className="carrito-icon-button"
-        onClick={toggleCarrito}
-      >
-        <Badge badgeContent={Object.values(resumen).length} color="error">
-          <ShoppingCartIcon fontSize="large" />
-        </Badge>
-      </IconButton>
+      <div>
+        <IconButton className="carrito-icon-button" onClick={toggleCarrito}>
+          <Badge badgeContent={Object.values(resumen).length} color="error">
+            <ShoppingCartIcon fontSize="large" />
+          </Badge>
+        </IconButton>
       </div>
 
       {/* Contenido del carrito */}
@@ -92,7 +86,10 @@ const Carrito: React.FC<CarritoProps> = ({ onPedidoConfirmado }) => {
                     </div>
                     <div className="carrito-item-precio">
                       ${(item.cantidad * item.precio).toFixed(2)}
-                      <IconButton onClick={() => eliminarItem(item.id)} className="carrito-eliminar-button">
+                      <IconButton
+                        onClick={() => eliminarItem(item.id)}
+                        className="carrito-eliminar-button"
+                      >
                         <DeleteIcon />
                       </IconButton>
                     </div>
@@ -104,8 +101,11 @@ const Carrito: React.FC<CarritoProps> = ({ onPedidoConfirmado }) => {
               <Typography variant="h6" className="carrito-total">
                 Total: ${calcularTotal().toFixed(2)}
               </Typography>
-              <Button onClick={handleConfirmarPedido} className="carrito-confirmar-button">
-                Confirmar Pedido 🛍️
+              <Button
+                onClick={handleConfirmarPedido}
+                className="carrito-confirmar-button"
+              >
+                Confirmar Pedido 🛍
               </Button>
             </>
           ) : (
